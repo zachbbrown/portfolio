9e2f7702bbbd30ec15abe49315d6af49462e5501
Zach Brown <zbrown8730@floridapoly.edu>
2025-02-16 22:42:51 +0000

    Welcome message

---
+++ welcome_code/welcome.c
@@ -0,0 +1,20 @@
+// One-line comment
+// This is the first program which prints a welcome message.
+
+/*
+
+Multiline comment. 
+
+January 9th 2025
+
+*/
+
+#include <stdio.h>
+
+// Create a main function
+int main() {
+    printf ("Welcome to my portfolio!\n");
+    printf ("I can't wait to show you what I am capable of.");
+
+    return 0;
+}
